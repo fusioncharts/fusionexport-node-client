3e075f94aa7ac14517f7ae5f50260b9751853eaa
Suvradip Saha <Suvradip@fusioncharts.com>
2020-02-21 14:03:19 +0530

    request module removed

--- src/ExportManager.js
+++ src/ExportManager.js
@@ -1,12 +1,14 @@
-// const WebSocket = require('ws');
-const path = require("path");
-const fs = require("fs-extra");
-const _ = require("lodash");
-const AdmZip = require("adm-zip");
-const tmp = require("tmp");
-const request = require("request");
-const { EventEmitter } = require("events");
-const config = require("./config.js");
+const path = require('path');
+const fs = require('fs-extra');
+const _ = require('lodash');
+const AdmZip = require('adm-zip');
+const tmp = require('tmp');
+const { URL } = require('url');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+const { EventEmitter } = require('events');
+const config = require('./config.js');
 
 class ExportManager extends EventEmitter {
   constructor(options) {
@@ -15,93 +17,138 @@ class ExportManager extends EventEmitter {
     this.config.url = `http://${this.config.host}:${this.config.port}/api/v2.0/export`;
   }
 
-  export(exportConfig, dirPath = ".", unzip = false) {
-    return new Promise((resolve, reject) => {
+  export(exportConfig, dirPath = '.', unzip = false) {
+    return new Promise(async (resolve, reject) => {
       const formData = _.cloneDeep(exportConfig.getFormattedConfigs());
       if (formData.payload) {
         formData.payload = fs.createReadStream(formData.payload);
       }
-      ExportManager.sendToServer(this.config.url, formData)
-        .then(content => {
-          const zipFile = ExportManager.saveZip(content);
-          const files = ExportManager.saveExportedFiles(zipFile, dirPath, unzip);
-          resolve(files);
-        })
-        .catch(err => reject(err));
+
+      try {
+        const content = await ExportManager.sendToServer(this.config.url, formData);
+        const zipFile = ExportManager.saveZip(content);
+        const files = ExportManager.saveExportedFiles(zipFile, dirPath, unzip);
+        resolve(files);
+      } catch (error) {
+        reject(error);
+      }
     });
   }
 
   exportAsStream(exportConfig) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const formData = _.cloneDeep(exportConfig.getFormattedConfigs());
       if (formData.payload) {
         formData.payload = fs.createReadStream(formData.payload);
       }
-      ExportManager.sendToServer(this.config.url, formData)
-        .then(content => {
-          const streams = ExportManager.unzipAsStream(content);
-          resolve(streams);
-        })
-        .catch(err => reject(err));
+
+      try {
+        const content = await ExportManager.sendToServer(this.config.url, formData);
+        const streams = ExportManager.unzipAsStream(content);
+        resolve(streams);
+      } catch (error) {
+        reject(error);
+      }
     });
   }
 
-  static sendToServer(serverUrl, formData) {
-    return new Promise((resolve, reject) => {
-      request.post(
-        {
-          url: serverUrl,
-          encoding: null,
-          formData,
-        },
-        (err, httpResponse, body) => {
-          if (err) {
-            if (err.code === "ECONNREFUSED") {
-              const connRefusedError = new Error(
-                `Unable to connect to FusionExport server. Make sure that your server is running on ${err.address}:${err.port}.`
-              );
-              connRefusedError.name = "Connection Refused";
-              reject(connRefusedError);
-              return;
-            }
+  static async sendToServer(serverUrl, formData) {
+    return new Promise(async (resolve, reject) => {
+      //   request.post(
+      //     {
+      //       url: serverUrl,
+      //       encoding: null,
+      //       formData,
+      //     },
+      //     (err, httpResponse, body) => {
+      //       if (err) {
+      //         if (err.code === 'ECONNREFUSED') {
+      //           const connRefusedError = new Error(
+      //             `Unable to connect to FusionExport server. Make sure that your server is running on ${err.address}:${err.port}.`
+      //           );
+      //           connRefusedError.name = 'Connection Refused';
+      //           reject(connRefusedError);
+      //           return;
+      //         }
 
-            reject(err);
+      //         reject(err);
+      //         return;
+      //       }
+
+      //       if (httpResponse.statusCode === 500) {
+      //         let errMsg = body.toString();
+
+      //         try {
+      //           errMsg = JSON.parse(errMsg).error;
+      //         } catch (e) {
+      //           // continue regardless of error
+      //         }
+
+      //         const serverError = new Error(errMsg);
+      //         serverError.name = 'Server Error';
+      //         reject(serverError);
+      //         return;
+      //       }
+
+      //       if (httpResponse.statusCode !== 200) {
+      //         reject(new Error(body.toString()));
+      //         return;
+      //       }
+
+      //       resolve(body);
+      //     }
+      //   );
+
+      const form = new FormData();
+      Object.keys(formData).forEach(key => {
+        form.append(key, formData[key]);
+      });
+
+      fetch(serverUrl, {
+        method: 'POST',
+        body: form,
+      })
+        .then(async res => {
+          if (res.status === 500) {
+            const { error = '' } = await res.json();
+            const serverError = new Error(error);
+            serverError.name = 'Server Error';
+            return reject(serverError);
+          }
+
+          if (res.status === 200) {
+            return resolve(await res.buffer());
+          }
+
+          if (res.status === 404) {
+            const notFoundError = new Error('API URL not found');
+            notFoundError.name = 'URL Not Found Error';
+            return reject(notFoundError);
+          }
+
+          return reject(new Error(await res.json().error));
+        })
+        .catch(err => {
+          if (err.code === 'ECONNREFUSED') {
+            const url = new URL(serverUrl);
+            const connRefusedError = new Error(
+              `Unable to connect to FusionExport server. Make sure that your server is running on ${url.host}`
+            );
+            connRefusedError.name = 'Connection Refused';
+            reject(connRefusedError);
             return;
           }
 
-          if (httpResponse.statusCode === 500) {
-            let errMsg = body.toString();
-
-            try {
-              errMsg = JSON.parse(errMsg).error;
-            } catch (e) {
-              // continue regardless of error
-            }
-
-            const serverError = new Error(errMsg);
-            serverError.name = "Server Error";
-            reject(serverError);
-            return;
-          }
-
-          if (httpResponse.statusCode !== 200) {
-            reject(new Error(body.toString()));
-            return;
-          }
-
-          resolve(body);
-        }
-      );
+          reject(err.message);
+        });
     });
   }
 
   static unzipAsStream(buf) {
     const zip = new AdmZip(buf);
     const zipEntries = zip.getEntries();
-
     const contents = {};
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < zipEntries.length; i++) {
+    for (let i = 0; i < zipEntries.length; i += 1) {
       const file = zip.readFile(zipEntries[i]);
       contents[zipEntries[i].entryName] = file;
     }
@@ -109,14 +156,14 @@ class ExportManager extends EventEmitter {
   }
 
   static saveZip(content) {
-    const zipFile = tmp.fileSync({ postfix: ".zip" });
+    const zipFile = tmp.fileSync({ postfix: '.zip' });
     fs.writeFileSync(zipFile.name, content);
     return zipFile.name;
   }
 
-  static saveExportedFiles(exportedFile, dirPath = ".", unzip = false) {
+  static saveExportedFiles(exportedFile, dirPath = '.', unzip = false) {
     if (!exportedFile) {
-      throw new Error("Exported files are missing");
+      throw new Error('Exported files are missing');
     }
 
     fs.ensureDirSync(dirPath);
@@ -129,7 +176,7 @@ class ExportManager extends EventEmitter {
       const extractedFiles = zip.getEntries().map(entry => path.resolve(dirPath, entry.entryName));
       savedFiles.push(...extractedFiles);
     } else {
-      const filename = "fusioncharts-export.zip";
+      const filename = 'fusioncharts-export.zip';
       const savedFile = path.resolve(dirPath, filename);
       fs.copySync(exportedFile, savedFile);
       savedFiles.push(savedFile);
